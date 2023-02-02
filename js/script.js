ea5122d9a918e540bc5a82809dd4743117243c84
Dmitry Shamko <proffery@gmail.com>
2023-02-02 23:50:12 +0300

    press keys

--- js/script.js
+++ js/script.js
@@ -7,15 +7,16 @@ function drawKeyboard() {
 function displayInput(e) {
     const pressedKey = document.querySelector(`[id="${e.target.id}"]`);
     const display = document.querySelector('.display');
-    pressedKey.classList.add('.key-click');
+    pressedKey.classList.add('key-click');
     display.textContent = `${e.target.id}`;
     console.log(e.target.id);
 }
 
 function removeAllTransition(e) {
     if (e.propertyName !== 'transform') return;
-    this.classList.remove('.key-click');
+    this.classList.remove('key-click');
 } 
+
 drawKeyboard();
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('click', displayInput));
