6b674c6b6c496f38e0b3510f40af36fe37606da8
Dmitry Shamko <proffery@gmail.com>
2023-02-06 02:13:33 +0300

    try to create array for operation and input digits

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 const keyboard = ['CE', '<-', '/', '*', '-', '7', '8', '9', '+', '4', '5', '6', '.', '1', '2', '3', '+/-', '0', '='];
 const keysContainer = document.querySelector('.keyboard');
-const regex = /^(\-|\d)(\d*)(\.?\d*)?$/;
+const regex = /((\-|\d)(\d*)(\.?\d*)?(\+|\-|\*|\/)?){1,10}/;
 const displayExpression = document.querySelector('.display-expression');
 const displayResult = document.querySelector('.display-result');
 let expression = '';
 let result = 0;
-
+let digitsArray = [];
+let operationArray = [];
+let digitsIter = 0;
+let opIter = 0;
 function drawKeyboard() {
     keysContainer.innerHTML = keyboard.map((key) => `<div class="key" id="${key}">${key}</div>`).join('');
 }
@@ -17,41 +20,128 @@ function displayInput(e) {
     if(e.target.id === 'CE') {
         clean();
     }
-    
     else if(e.target.id === '<-') {
         remove();
     }
-
     else if(e.target.id === '+/-') {
         plusMinus();
     }
-
     else if(e.target.id === '-') {
         minus(e);
+        createOperationArray(e.target.id);
+        createDigitsArray(e.target.id);
     }
-
     else if(e.target.id === '+') {
         plus(e);
+        createOperationArray(e.target.id);
+        createDigitsArray(e.target.id);
     }
-
     else if(e.target.id === '*') {
         multiply(e);
+        createOperationArray(e.target.id);
+        createDigitsArray(e.target.id);
     }
-
     else if(e.target.id === '/') {
-        division(e);
+        divide(e);
+        createOperationArray(e.target.id);
+        createDigitsArray(e.target.id);
     }
-
     else if(e.target.id === '.') {
         dot(e);
     }
-
+    else if(e.target.id === '=') {
+        createDigitsArray(e.target.id);
+        calculate();
+    }
     else {
         digitsInput(e);
     }
-    
+    calculate();
 }
 
+function calculate() {
+    if (operationArray.length == 0 || digitsArray.length < 2) {
+        return;
+    }
+
+    else {
+        for (let i = 0; i < digitsArray.length; i++) {
+            if (operationArray[i] === '-') {
+                result = digitsArray[i] - digitsArray[i + 1];
+            }
+            if (operationArray[i] === '+') {
+                result = digitsArray[i] + digitsArray[i + 1];
+            }
+            if (operationArray[i] === '*') {
+                result = digitsArray[i] * digitsArray[i + 1];
+            }
+            if (operationArray[i] === '/') {
+                result = digitsArray[i] / digitsArray[i + 1];
+            }  
+        }
+    }
+    displayResult.textContent = result;
+}
+
+function createOperationArray(id) {
+    operationArray[opIter] = id;
+    console.log(operationArray);
+    console.log('iterOper:'+ opIter);
+    opIter++;
+}
+
+function createDigitsArray(id) {
+    if (id === '=') {
+        let tempArr = [];
+        tempArr = expression.split(operationArray[operationArray.length - 1]);
+        console.log(tempArr);
+        digitsArray[digitsIter] = tempArr[tempArr.length - 1].substring(0, tempArr[tempArr.length - 1].length - 1);
+    }
+
+    if (digitsIter < 1) {
+        console.log(digitsArray);
+        console.log('iterDigits:'+ digitsIter);
+        digitsIter++;
+        return;
+    }
+
+    else if (digitsIter == 1){
+        if (operationArray[operationArray.length - 1] == operationArray[operationArray.length - 2]) {
+            digitsArray = expression.split(operationArray[operationArray.length - 1]);
+            digitsArray.pop();
+            console.log(digitsArray);
+        }
+        else {
+            digitsArray = expression.split(operationArray[operationArray.length - 2]);
+            digitsArray[digitsIter] = digitsArray[digitsIter].substring(0, digitsArray[digitsIter].length - 1);
+            console.log(digitsArray);
+        }
+    }
+
+    else {
+        if (operationArray[operationArray.length - 1] == operationArray[operationArray.length - 2]) {
+            let tempArr = [];
+            tempArr = expression.split(operationArray[operationArray.length - 1]);
+            tempArr.pop();
+            digitsArray[digitsIter] = tempArr[tempArr.length - 1];
+            console.log(tempArr);
+            console.log(digitsArray);
+        }
+        else {
+            let tempArr = [];
+            tempArr = expression.split(operationArray[operationArray.length - 2]);
+            console.log(tempArr);
+            digitsArray[digitsIter] = tempArr[tempArr.length - 1].substring(0, tempArr[tempArr.length - 1].length - 1);
+            console.log(digitsArray);
+        }
+    }
+
+    // else {
+
+    // }
+    console.log('iterDigits:'+ digitsIter);
+    digitsIter++;
+}
 
 function digitsInput(e) {
     console.log(`digits input: ${e.target.id}`);
@@ -60,19 +150,27 @@ function digitsInput(e) {
 
 function clean() {
     console.log('clean:');
-    console.log('expression before: '+expression);
-    console.log('result before: '+result);
     expression = '';
     result = 0;
-    console.log('expression after: '+expression);
-    console.log('result after: '+result);
+    console.log('expression: '+ expression);
+    console.log('result: '+ result);
+    opIter = 0;
+    digitsIter = 0;
+    for (let i = 0; i < operationArray.length; i++) {
+        operationArray.pop();
+    }
+    for (let j = 0; j < digitsArray.length; j++) {
+        digitsArray.pop();
+    }
+    console.log('digitsArr: '+ digitsArray);
+    console.log('operationArray: '+ operationArray);
     displayExpression.textContent = expression;
     displayResult.textContent = result;
 }
 
 function remove() {
     console.log('remove:');
-    console.log('expression before: '+expression);
+    console.log('expression before: '+ expression);
     expression = expression.substring(0, expression.length - 1);
     console.log('expression after: '+ expression);
     displayExpression.textContent = expression;
@@ -101,7 +199,7 @@ function multiply(e) {
     cleanExpressionEnter(e.target.id);
 }
 
-function division(e) {
+function divide(e) {
     console.log('division:');
     cleanExpressionEnter(e.target.id);
 }
@@ -110,6 +208,7 @@ function dot(e) {
     console.log('dot:');
     cleanExpressionEnter(e.target.id);
 }
+
 function cleanExpressionEnter(id) {
     console.log('expression before: '+ expression);
     expression = expression.concat(id);
